e934ddc7b7601d4fe5e6cdd5de4344cd980beab5
y97523 <y97523@6d272bb8-ed2a-0410-9301-61256c6c78f4>
2007-05-13 06:07:54 +0000

    done: 目录根据初始录入日期改变 todo: 无权限的定制提示页 报表

--- yxwz.master.cs
+++ yxwz.master.cs
@@ -8,6 +8,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using System.Web.Configuration;
 
 public partial class yxwz : System.Web.UI.MasterPage
 {
@@ -17,9 +18,13 @@ public partial class yxwz : System.Web.UI.MasterPage
     }
     protected void TreeView1_Load(object sender, EventArgs e)
     {
-        //if (!IsPostBack)
-        //{
-        //    TreeView1.
-        //}
+        if (DateTime.Now > DateTime.Parse(WebConfigurationManager.AppSettings.Get("初始录入结束时间")))
+        {
+            TreeView1.Nodes.RemoveAt(0);
+        }
+        else
+        {
+            TreeView1.Nodes.RemoveAt(1);
+        }
     }
 }
